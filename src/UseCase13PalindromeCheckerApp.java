c5902e868782db43c83151d7a279dcbd10a6e4a3
baibhab10-oct <baibhab05@gmail.com>
2026-03-06 10:55:13 +0530

    UC13:Performance Comparison

--- src/UseCase13PalindromeCheckerApp.java
+++ src/UseCase13PalindromeCheckerApp.java
@@ -1,30 +1,27 @@
 import java.util.Stack;
-
-public class UseCase12PalindromeCheckerApp {
+public class UseCase13PalindromeCheckerApp {
     public static void main(String[] args) {
         String input = "level";
-
         PalindromeStrategy strategy = new StackStrategy();
+        long startTime = System.nanoTime();
         boolean isPalindrome = strategy.check(input);
-
+        long endTime = System.nanoTime();
+        long executionTime = endTime - startTime;
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + executionTime + " ns");
     }
 }
-
 interface PalindromeStrategy {
     boolean check(String input);
 }
-
 class StackStrategy implements PalindromeStrategy {
     @Override
     public boolean check(String input) {
         Stack<Character> stack = new Stack<>();
-
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
-
         for (char c : input.toCharArray()) {
             if (c != stack.pop()) {
                 return false;
